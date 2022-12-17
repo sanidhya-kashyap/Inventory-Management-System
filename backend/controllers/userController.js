0b61d7a2aa0beb2985290d6996d1e32e7bc9b923
sanidhya-kashyap <sanidhyak7@gmail.com>
2022-12-17 23:58:05 +0530

    updated get user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,90 +1,156 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const JWT = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const generateToken = (id) => {
-    return JWT.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 //register user
 
-const registerUser =  asyncHandler( async (req, res) => {
-   const {name, email, password} = req.body
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-   //Validation 
-   if (!name || !email || !password) {
-    res.status(400)
-    throw new Error("Please fill in all require fields")
-   }
-   if (password.length < 6) {
-    res.status(400)
-    throw new Error("Password must be greater than 6 characters")
-   }
+  //Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all require fields");
+  }
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("Password must be greater than 6 characters");
+  }
 
-   // chcek if user email already exists
-   const userExists = await User.findOne({email})
+  // chcek if user email already exists
+  const userExists = await User.findOne({ email });
 
-   if (userExists) {
-    res.status(400)
-    throw new Error("Email already exists")
-   }
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email already exists");
+  }
 
-
-   // create new user
-   const user = await User.create({
+  // create new user
+  const user = await User.create({
     name,
     email,
     password,
-   });
+  });
 
-      // generate token 
-      const token = generateToken(user._id);
+  // generate token
+  const token = generateToken(user._id);
 
-      //send HTTP-only cookie 
-      res.cookie("token", token, {
-        path:  "/",
-        httpOnly: true,
-        expires:new Date(Date.now() + 1000 * 86400),  // 1 day
-        sameSite: "none",
-        secure: true
-      })
+  //send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), // 1 day
+    sameSite: "none",
+    secure: true,
+  });
 
-   if (user) {
-    const {_id, name, email, photo, phone, bio} = user
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
-        _id, name, email, photo, phone, bio, token,
-        
-    })
-   } else {
-    res.status(400) 
-    throw new Error("Invalid User Data") 
-   }
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
 });
 
 // Login user
-const loginUser = asyncHandler( async (req, res) => {
-    const {email, password} = req.body
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    // validate request 
-    if (!email || !password) {
-        res.status(400);
-        throw new Error("Please add email and password");
-    }
-    // Check if user exists
-    const user = await User.findOne({email})
-    if (!user) {
-        res.status(400);
-        throw new Error("User not found, Please sign up");
-    }
+  // validate request
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add email and password");
+  }
+  // Check if user exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, Please sign up");
+  }
 
-    // Password correct or not
+  // Password correct or not
 
-    const passwordIsCorect = await bcrypt.compare(password, )
-     
+  const passwordIsCorect = await bcrypt.compare(password, user.password);
+
+  // generate token
+  const token = generateToken(user._id);
+
+  //send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), // 1 day
+    sameSite: "none",
+    secure: true,
+  });
+
+  if (user && passwordIsCorect) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+//Logout User
+const logOut = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+  });
+  return res.status(200).json({ message: "successfully logged out" });
+});
+
+//getuser data
+
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
 });
 
 module.exports = {
-    registerUser,
-    loginUser,
-}
+  registerUser,
+  loginUser,
+  logOut,
+  getUser,
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const { registerUser, loginUser } = require("../controllers/userController");
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logOut,
+  getUser,
+} = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logOut);
+router.get("/getuser", protect, getUser);
 
-module.exports = router; 
+module.exports = router;
